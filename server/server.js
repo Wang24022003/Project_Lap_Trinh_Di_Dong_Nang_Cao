6c2c5c987052f83d67fafbca11741bb2c19c370a
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-09-16 19:32:30 +0700

    upload server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+//rest object
+const app = express();
+
+//route
+app.get("/", (req, res) => {
+     return res.status(200).send("<h1>Welcome To Node server</h1>");
+});
+
+//port
+const PORT = 8000;
+
+//listen
+app.listen(PORT, () => {
+     console.log("Server Running");
+});
