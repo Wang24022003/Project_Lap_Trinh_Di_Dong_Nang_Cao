dd1ea0162adde3052bdba8f0f8a0425223c87bb5
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-09-18 11:20:39 +0700

    update server payment

---
+++ .history/server/controllers/orderController_20240918100735.js
@@ -0,0 +1,111 @@
+import orderModel from "../models/orderModel.js";
+import productModel from "../models/productModel.js";
+
+// CREATE ORDERS
+export const createOrderController = async (req, res) => {
+  try {
+    const {
+      shippingInfo,
+      orderItems,
+      paymentMethod,
+      paymentInfo,
+      itemPrice,
+      tax,
+      shippingCharges,
+      totalAmount,
+    } = req.body;
+    //valdiation
+    // create order
+    await orderModel.create({
+      user: req.user._id,
+      shippingInfo,
+      orderItems,
+      paymentMethod,
+      paymentInfo,
+      itemPrice,
+      tax,
+      shippingCharges,
+      totalAmount,
+    });
+
+    // stock update
+    for (let i = 0; i < orderItems.length; i++) {
+      // find product
+      const product = await productModel.findById(orderItems[i].product);
+      product.stock -= orderItems[i].quantity;
+      await product.save();
+    }
+    res.status(201).send({
+      success: true,
+      message: "Order Placed Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Create Order API",
+      error,
+    });
+  }
+};
+// GET ALL ORDERS - MY ORDERS
+export const getMyOrdersCotroller = async (req, res) => {
+     try {
+       // find orders
+       const orders = await orderModel.find({ user: req.user._id });
+       //valdiation
+       if (!orders) {
+         return res.status(404).send({
+           success: false,
+           message: "no orders found",
+         });
+       }
+       res.status(200).send({
+         success: true,
+         message: "your orders data",
+         totalOrder: orders.length,
+         orders,
+       });
+     } catch (error) {
+       console.log(error);
+       res.status(500).send({
+         success: false,
+         message: "Error In My orders Order API",
+         error,
+       });
+     }
+   };
+   
+   // GET SINGLE ORDER INFO
+   export const singleOrderDetrailsController = async (req, res) => {
+     try {
+       // find orders
+       const order = await orderModel.findById(req.params.id);
+       //valdiation
+       if (!order) {
+         return res.status(404).send({
+           success: false,
+           message: "no order found",
+         });
+       }
+       res.status(200).send({
+         success: true,
+         message: "your order fetched",
+         order,
+       });
+     } catch (error) {
+       console.log(error);
+       // cast error ||  OBJECT ID
+       if (error.name === "CastError") {
+         return res.status(500).send({
+           success: false,
+           message: "Invalid Id",
+         });
+       }
+       res.status(500).send({
+         success: false,
+         message: "Error In Get UPDATE Products API",
+         error,
+       });
+     }
+   };
---
+++ .history/server/routes/orderRoutes_20240918100354.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { isAuth } from "./../middlewares/authMiddleware.js";
+
+import {
+  createOrderController,
+  getMyOrdersCotroller,
+  singleOrderDetrailsController,
+} from "../controllers/orderController.js";
+
+const router = express.Router();
+
+//rroutes
+// ============== ORDERS ROUTES ==================
+//rroutes
+// ============== ORDERS ROUTES ==================
+//rroutes
+// ============== ORDERS ROUTES ==================
+//rroutes
+// ============== ORDERS ROUTES ==================
+// CREATE ORDERS
+router.post("/create", isAuth, createOrderController);
+
+//  GET ALL ORDERS
+router.get("/my-orders", isAuth, getMyOrdersCotroller);
+
+//  GET SINGLE ORDER DETAILS
+router.get("/my-orders/:id", isAuth, singleOrderDetrailsController);
+
+// ====================================================================
+
+export default router;
